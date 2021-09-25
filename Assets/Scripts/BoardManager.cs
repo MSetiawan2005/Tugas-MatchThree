7dfba02bf3cb1784d7a2a9111cb0569823d0325c
MSetiawan2005 <ms0132005@gmail.com>
2021-09-25 10:01:43 +0700

    Games Match Three

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,432 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    private int combo;
+
+    private static BoardManager _instance = null;
+    public static BoardManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<BoardManager>();
+            }
+
+            return _instance;
+        }        
+    }
+
+    [Header("Board")]
+    public Vector2Int size;
+    public Vector2 offsetTile;
+    public Vector2 offsetBoard;
+
+    [Header("Tile")]
+    public List<Sprite> tileTypes = new List<Sprite>();
+    public GameObject tilePrefab;
+    
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private TileController[,] tiles;
+
+    public bool IsSwapping { get; set; }
+    public bool IsProcessing { get; set; }
+    public bool IsAnimating
+    {
+        get
+        {
+            return IsProcessing || IsSwapping;
+        }
+    }
+    public bool IsAllTrue(List<bool> list)
+    {
+        foreach(bool status in list)
+        {
+            if (!status) return false;
+        }
+
+        return true;
+    }
+
+
+    private void Start()
+    {
+        Vector2 tileSize = tilePrefab.GetComponent<SpriteRenderer>().size;
+        CreateBoard(tileSize);
+
+        IsProcessing = false;
+        IsSwapping = false;
+    }
+
+    private void CreateBoard(Vector2 tileSize)
+    {
+        tiles = new TileController[size.x, size.y];
+        Vector2 totalSize = (tileSize + offsetTile) * (size - Vector2.one);
+
+        startPos = (Vector2)transform.position - (totalSize / 2) + offsetBoard;
+        endPos = startPos + totalSize;
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+
+                TileController newTile = Instantiate(
+                    tilePrefab,
+                    new Vector2(
+                        startPos.x + (tileSize.x + offsetTile.x) * x,
+                        startPos.y + (tileSize.y + offsetTile.y) * y),
+                    tilePrefab.transform.rotation, transform).GetComponent<TileController>();
+                
+                tiles[x, y] = newTile;
+
+                List<int> possibleId = GetStartingPossibleList(x, y);
+                int newID = possibleId[Random.Range(0, possibleId.Count)];
+
+                newTile.GivePower();
+                newTile.ChangeId(newID, x, y);                
+            }
+        }
+    }    
+
+    private List<int> GetStartingPossibleList(int x, int y)
+    {
+        List<int> possibleId = new List<int>();
+
+        for (int i=0; i< tileTypes.Count; i++)
+        {
+            possibleId.Add(i);
+        }
+
+        if (x > 1 && tiles[x-1, y].id == tiles[x - 2, y].id)
+        {
+            possibleId.Remove(tiles[x - 1, y].id);
+        }
+
+        if (y > 1 && tiles[x, y-1].id == tiles[x, y - 2].id)
+        {
+            possibleId.Remove(tiles[x, y - 1].id);
+        }
+
+        return possibleId;
+    }
+
+    public Vector2Int GetTileIndex(TileController tile)
+    {
+        for (int x =0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                if (tile == tiles[x, y]) return new Vector2Int(x, y);
+            }
+        }
+
+        return new Vector2Int(-1, -1);
+    }
+
+    public Vector2 GetIndexPos(Vector2Int index)
+    {
+        Vector2 tileSize = tilePrefab.GetComponent<SpriteRenderer>().size;
+        return new Vector2(
+            startPos.x + ((tileSize.x + offsetTile.x) * index.x),
+            startPos.y + ((tileSize.y + offsetTile.y) * index.y)
+            );
+    }   
+    
+    #region Swapping
+    public IEnumerator SwapTilePosition(TileController a, TileController b, System.Action onCompleted)
+    {
+        IsSwapping = true;
+
+        Vector2Int indexA = GetTileIndex(a);
+        Vector2Int indexB = GetTileIndex(b);
+
+        tiles[indexA.x, indexA.y] = b;
+        tiles[indexB.x, indexB.y] = a;
+
+        a.ChangeId(a.id, indexB.x, indexB.y);
+        b.ChangeId(b.id, indexA.x, indexA.y);
+
+        bool isRoutineACompleted = false;
+        bool isRoutineBCompleted = false;
+
+        StartCoroutine(
+            a.MoveTilePosition(GetIndexPos(indexB), () => { isRoutineACompleted = true; }
+            ));
+
+        StartCoroutine(
+            b.MoveTilePosition(GetIndexPos(indexA), () => { isRoutineBCompleted = true; }
+            ));
+
+        yield return new WaitUntil(() =>
+        {
+            return isRoutineACompleted && isRoutineBCompleted;
+        });
+
+        onCompleted?.Invoke();
+        IsSwapping = false;
+    }
+    #endregion
+
+    #region Match
+
+    public List<TileController> GetAllMatches()
+    {
+        List<TileController> matchingTiles = new List<TileController>();
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                List<TileController> tileMatched = tiles[x, y].GetAllMatches();
+
+                if (tileMatched == null || tileMatched.Count == 0) continue;
+
+                foreach (TileController item in tileMatched)
+                {
+
+                    // kalau tile yang ke match punya power, maka satu baris searah dengan directionya akan ditambahkan kedalam matchingTiles
+                    if (item.HasPower)
+                    {
+                        List<TileController> additionalTiles = new List<TileController>();
+                        if (item.tilePower == TilePower.Horizontal)
+                        {
+                            additionalTiles.AddRange(item.GetLineTiles(new Vector2[2] { Vector2.left, Vector2.right }));
+                        } else if (item.tilePower == TilePower.Vertical)
+                        {
+                            additionalTiles.AddRange(item.GetLineTiles(new Vector2[2] { Vector2.up, Vector2.down }));
+                        }
+
+                        if (additionalTiles.Count > 0)
+                        {
+                            matchingTiles.AddRange(additionalTiles);
+                        }
+                    }
+
+                    if (!matchingTiles.Contains(item))
+                    {
+                        matchingTiles.Add(item);
+                    }
+
+
+                }
+            }
+        }
+
+        return matchingTiles;
+    }
+
+    public void Process()
+    {
+        IsProcessing = true;
+        ProcessMatch();
+        combo = 0;
+    }
+
+    private void ProcessMatch()
+    {
+        List<TileController> matchingTiles = GetAllMatches();
+
+        if (matchingTiles == null || matchingTiles.Count == 0)
+        {
+            IsProcessing = false;
+            return;
+        }
+
+        combo++;
+
+        ScoreManager.Instance.IncrementCurrentScore(matchingTiles.Count, combo);
+        StartCoroutine(ClearMatches(matchingTiles, ProcessDrop));
+    }
+
+    private IEnumerator ClearMatches(List<TileController> matchingTiles, System.Action onCompleted)
+    {
+        List<bool> isCompleted = new List<bool>();
+
+        for(int i = 0; i < matchingTiles.Count; i++)
+        {
+            isCompleted.Add(false);
+        }
+
+        for(int i = 0; i < matchingTiles.Count; i++)
+        {
+            int index = i;
+            StartCoroutine(matchingTiles[i].SetDestroyed(() =>
+            {
+                isCompleted[index] = true;
+            }));
+        }
+
+        yield return new WaitUntil(() =>
+        {
+            return IsAllTrue(isCompleted);
+        });
+
+        onCompleted?.Invoke();
+    }
+
+    private void ProcessDrop()
+    {
+        Dictionary<TileController, int> droppingTiles = GetAllDrop();
+        StartCoroutine(DropTiles(droppingTiles, ProcessDestroyAndFill));
+    }
+
+    private Dictionary<TileController, int> GetAllDrop()
+    {
+        Dictionary<TileController, int> droppingTiles = new Dictionary<TileController, int>();
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y= 0; y< size.y; y++)
+            {
+                if (tiles[x, y].IsDestroyed)
+                {
+                    for (int i = y + 1; i < size.y; i++)
+                    {                        
+                        if (tiles[x, i].IsDestroyed)
+                        {
+                            continue;
+                        }
+
+                        if (droppingTiles.ContainsKey(tiles[x, i]))
+                        {
+                            droppingTiles[tiles[x, i]]++;
+                        } else
+                        {
+                            droppingTiles.Add(tiles[x, i], 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        return droppingTiles;
+    }
+
+    private IEnumerator DropTiles(Dictionary<TileController, int> droppingTiles, System.Action onCompleted)
+    {
+        foreach (KeyValuePair<TileController, int> pair in droppingTiles) {
+            Vector2Int tileIndex = GetTileIndex(pair.Key);
+
+            TileController temp = pair.Key;
+            tiles[tileIndex.x, tileIndex.y] = tiles[tileIndex.x, tileIndex.y - pair.Value];
+            tiles[tileIndex.x, tileIndex.y - pair.Value] = temp;
+
+            temp.ChangeId(temp.id, tileIndex.x, tileIndex.y - pair.Value);
+        }
+
+        yield return null;
+
+        onCompleted?.Invoke();
+    }
+    #endregion
+
+    #region Tile Power
+
+    #endregion
+
+    #region Destroy & Fill
+
+    private void ProcessDestroyAndFill()
+    {
+        List<TileController> destroyedTiles = GetAllDestroyed();
+        StartCoroutine(DestroyedAndFillTiles(destroyedTiles, ProcessReposition));
+
+    }
+
+    private List<TileController> GetAllDestroyed()
+    {
+        List<TileController> destroyedTiles = new List<TileController>();
+
+        for(int x = 0; x < size.x; x++)
+        {
+            for (int y=0; y < size.y; y++)
+            {
+                if (tiles[x, y].IsDestroyed)
+                {
+                    destroyedTiles.Add(tiles[x, y]);
+                }
+            }
+        }
+
+        return destroyedTiles;
+    }
+
+    private IEnumerator DestroyedAndFillTiles(List<TileController> destroyedTiles, System.Action onCompleted)
+    {        
+        List<int> highestIndex = new List<int>();
+
+        for (int i = 0; i< size.x; i++)
+        {
+            highestIndex.Add(size.y - 1);
+        }
+
+        float spawnHeight = endPos.y + tilePrefab.GetComponent<SpriteRenderer>().size.y + offsetTile.y;
+
+        foreach(TileController tile in destroyedTiles)
+        {
+            Vector2Int tileIndex = GetTileIndex(tile);
+            Vector2Int targetIndex = new Vector2Int(tileIndex.x, highestIndex[tileIndex.x]);
+            highestIndex[tileIndex.x]--;
+
+            tile.transform.position = new Vector2(tile.transform.position.x, spawnHeight);
+            tile.GenerateRandomTiles(targetIndex.x, targetIndex.y);
+        }
+
+        yield return null;
+
+        onCompleted?.Invoke();
+    }
+
+    #endregion
+
+    #region Reposition
+
+    private void ProcessReposition()
+    {
+        StartCoroutine(RepositionTiles(ProcessMatch));
+    }
+
+    private IEnumerator RepositionTiles(System.Action onCompleted)
+    {
+        List<bool> isCompleted = new List<bool>();
+
+        int i = 0;
+        for(int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                Vector2 targetPos = GetIndexPos(new Vector2Int(x, y));                
+                if ((Vector2)tiles[x,y].transform.position == targetPos)
+                {
+                    continue;
+                }
+
+                isCompleted.Add(false);
+
+                int index = i;
+                StartCoroutine(tiles[x, y].MoveTilePosition(targetPos, () =>
+                 {
+                     isCompleted[index] = true;
+                 }));
+
+                //increment
+                i++;
+            }
+        }
+
+        yield return new WaitUntil(() =>
+        {
+            return IsAllTrue(isCompleted);
+        });
+        onCompleted?.Invoke();
+    }
+
+    #endregion
+
+   
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,332 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TilePower { None, Horizontal, Vertical, Explode }
+
+public class TileController : MonoBehaviour
+{
+    public int id;
+    
+    public float powerChance = 0.05f;
+
+    private BoardManager board;
+    private SpriteRenderer sprRenderer;
+
+    private static readonly Color SelectedColor = new Color(0.5f, 0.5f, 0.5f);
+    private static readonly Color normalColor = Color.white;
+    private static readonly float moveDuration = 0.5f;
+    private static readonly float destroyBigDuration = 0.1f;
+    private static readonly float destroySmallDuration = 0.4f;
+
+    private static readonly Vector2 sizeBig = Vector2.one * 1.2f;
+    private static readonly Vector2 sizeSmall = Vector2.zero;
+    private static readonly Vector2 sizeNormal = Vector2.one;
+    private static readonly Vector2[] adjacentDirection = new Vector2[]
+    {
+        Vector2.up, Vector2.down, Vector2.left, Vector2.right
+    };
+    private static TileController previousSelected = null;
+
+    private GameFlowManager game;
+    public TilePower tilePower;
+
+    [SerializeField] private List<GameObject> _powers;
+
+    private bool isSelected = false;
+
+    public bool IsDestroyed { get; private set; }
+
+    public bool HasPower { get { return (tilePower != TilePower.None); } }
+
+    private void Start()
+    {
+        IsDestroyed = false;        
+    }
+
+    private void Awake()
+    {
+        board = BoardManager.Instance;
+        sprRenderer = GetComponent<SpriteRenderer>();
+        game = GameFlowManager.Instance;
+    }
+
+    private void OnMouseDown()
+    {
+        if (sprRenderer.sprite == null || board.IsAnimating || game.IsGameOver) return;
+
+        SoundManager.Instance.PlayTap();
+
+        if (isSelected) {
+            Deselect();
+        }
+        else {
+
+            if (previousSelected == null) {
+                Select();
+            }
+            else {
+                if (GetAllAdjacentTiles().Contains(previousSelected)) {
+                    TileController otherTile = previousSelected;
+                    previousSelected.Deselect();
+
+                    SwapTile(otherTile, () => {
+                        if (board.GetAllMatches().Count > 0) {
+                            // match found
+                            board.Process();
+                        }
+                        else {
+                            SoundManager.Instance.PlayWrong();
+                            SwapTile(otherTile);
+                        }
+                    });
+
+                }
+                else {
+                    previousSelected.Deselect();
+                    Select();
+                }
+            }
+
+        }
+    }
+
+    public void GivePower()
+    {
+        foreach (GameObject item in _powers)
+        {
+            item.SetActive(false);
+        }
+        float rand = Random.Range(0f, 1f);
+        if (rand < powerChance)
+        {
+            tilePower = (TilePower)Random.Range(1, 3);
+            _powers.Find(e => e.name.Contains(tilePower.ToString())).SetActive(true);            
+        }
+        else
+        {
+            tilePower = TilePower.None;            
+        }  
+    }
+
+    public void ChangeId(int id, int x, int y)
+    {
+        sprRenderer.sprite = board.tileTypes[id];
+        this.id = id;
+
+        name = "TILE_" + id + "("+x+","+y+")";
+    }
+
+    public void SwapTile(TileController otherTile, System.Action onCompleted = null)
+    {
+        StartCoroutine(board.SwapTilePosition(this, otherTile, onCompleted));
+    }
+
+    public void GenerateRandomTiles(int x, int y)
+    {
+        transform.localScale = sizeNormal;
+        IsDestroyed = false;
+        GivePower();
+        ChangeId(Random.Range(0, board.tileTypes.Count), x, y);        
+    }
+
+    public IEnumerator MoveTilePosition(Vector2 targetPos, System.Action onCompleted)
+    {
+        Vector2 startPos = transform.position;
+        float time = 0.0f;
+
+        yield return new WaitForEndOfFrame();
+
+        while(time < moveDuration) {
+            transform.position = Vector2.Lerp(startPos, targetPos, time / moveDuration);
+            time += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        transform.position = targetPos;
+
+        onCompleted?.Invoke();
+    }
+
+    public IEnumerator SetDestroyed(System.Action onCompleted)
+    {
+        IsDestroyed = true;
+        id = -1;
+        name = "TILE_NULL";
+
+        Vector2 startSize = transform.localScale;
+        float time = 0.0f;
+        while (time < destroyBigDuration)
+        {
+            transform.localScale = Vector2.Lerp(startSize, sizeSmall, time / destroySmallDuration);
+            time += Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.localScale = sizeSmall;
+
+        sprRenderer.sprite = null;
+        onCompleted?.Invoke();
+    }
+
+    #region Select & Deselect
+
+    private void Select()
+    {
+        isSelected = true;
+        sprRenderer.color = SelectedColor;
+        previousSelected = this;
+    }
+
+    private void Deselect()
+    {
+        isSelected = false;
+        sprRenderer.color = normalColor;
+        previousSelected = null;
+    }
+    #endregion
+
+    #region Adjacent
+    private TileController GetAdjacent(Vector2 castDir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir, sprRenderer.size.x);
+
+        if (hit)
+        {
+            return hit.collider.GetComponent<TileController>();
+        }
+
+        return null;
+    }
+
+    public List<TileController> GetAllAdjacentTiles()
+    {
+        List<TileController> adjacentTiles = new List<TileController>();
+
+        for (int i =0; i < adjacentDirection.Length; i++)
+        {
+            adjacentTiles.Add(GetAdjacent(adjacentDirection[i]));
+        }
+
+        return adjacentTiles;
+    }
+    #endregion
+
+    #region Check Match
+
+    private List<TileController> GetMatch(Vector2 castDir)
+    {
+        List<TileController> matchingTiles = new List<TileController>();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir, sprRenderer.size.x);
+
+        while (hit)
+        {
+            TileController otherTile = hit.collider.GetComponent<TileController>();
+            if ((otherTile.id != id || otherTile.IsDestroyed))
+            {
+                break;
+            }
+
+            matchingTiles.Add(otherTile);
+            hit = Physics2D.Raycast(otherTile.transform.position, castDir, sprRenderer.size.x);
+
+        }
+
+        return matchingTiles;
+    }
+
+    private List<TileController> GetOneLineMatch(Vector2[] paths)
+    {
+        List<TileController> matchingTiles = new List<TileController>();
+
+        for (int i = 0; i < paths.Length; i++)
+        {
+            matchingTiles.AddRange(GetMatch(paths[i]));
+        }
+
+        if (matchingTiles.Count >= 2)
+        {
+            return matchingTiles;
+        }
+
+        return null;
+    }
+
+    public List<TileController> GetAllMatches()
+    {
+        if (IsDestroyed)
+        {
+            return null;
+        }
+
+        List<TileController> matchingTiles = new List<TileController>();
+
+        List<TileController> horizontalMatches = GetOneLineMatch(new Vector2[2] { Vector2.up, Vector2.down });
+        List<TileController> verticalMatches = GetOneLineMatch(new Vector2[2] { Vector2.left, Vector2.right });
+
+        if (horizontalMatches != null)
+        {
+            matchingTiles.AddRange(horizontalMatches);
+        }
+
+        if (verticalMatches != null)
+        {
+            matchingTiles.AddRange(verticalMatches);
+        }
+
+        if (matchingTiles!= null && matchingTiles.Count >= 2)
+        {
+            matchingTiles.Add(this);
+        }
+
+        return matchingTiles;
+    }
+
+    #endregion
+
+    #region Power
+
+
+    // melakukan Raycasting mirip seperti GetAllMatches namun tanpa batasan harus memiliki id yang sama
+
+    public List<TileController> GetLineTiles(Vector2[] paths) {
+        TilePower casterPower = tilePower;
+        List<TileController> tilesInline = new List<TileController>();
+
+        for (int i = 0; i < paths.Length; i++)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, paths[i], sprRenderer.size.x);
+            
+            while (hit)
+            {                
+                TileController otherTile = hit.collider.GetComponent<TileController>();
+
+                if (otherTile.IsDestroyed) break;
+
+                if (otherTile.HasPower && otherTile.tilePower != casterPower)
+                {
+                    List<TileController> additionalTiles = new List<TileController>();
+                    
+
+                    // jika tile yang di hit memiliki power (chain reaction) maka akan memanggil fungsi ini lagi dari perspektif tile yang di hit(otherTile)
+                    if (otherTile.tilePower == TilePower.Horizontal)
+                    {
+                        additionalTiles.AddRange(otherTile.GetLineTiles(new Vector2[2] { Vector2.left, Vector2.right }));
+                    } else if (otherTile.tilePower == TilePower.Vertical) {
+                        additionalTiles.AddRange(otherTile.GetLineTiles(new Vector2[2] { Vector2.up, Vector2.down }));
+                    }
+
+                    tilesInline.AddRange(additionalTiles);
+
+                }
+
+                tilesInline.Add(otherTile);
+                hit = Physics2D.Raycast(otherTile.transform.position, paths[i], sprRenderer.size.x);
+            }
+        }
+
+        return tilesInline;
+    }
+
+    #endregion
+}
